12d50ebd5f426152ff3c76b591a32ae54d45035e
Konstantin Mokievets <k.mokievets@gmail.com>
2024-04-22 14:41:14 +0300

    tasks added

--- 01_multi_args.py
+++ 01_multi_args.py
@@ -1,3 +1,15 @@
+'''
+Вам нужно реализовать функцию greet(), которая должна принимать несколько имён (как минимум одно!) и возвращать строку приветствия (см. примеры ниже).
+
+greet('Bob')
+# 'Hello, Bob!'
+greet('Moe', 'Mary')
+# 'Hello, Moe and Mary!'
+greet(*'ABC')
+# 'Hello, A and B and C!'
+
+'''
+
 def greet(x, *args):
     new = (x,) + args #new tuple with all names (x + args)
     print(f'Hello, ' + ' and '.join(new) + '!')    
